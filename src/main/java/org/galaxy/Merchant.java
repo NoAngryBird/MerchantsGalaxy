38299135a464d64bf5b697ad730601ed709fd391
Hannes Helberg <hh@frei.one>
2023-11-14 20:55:05 +0200

    Added conversion from galactic to roman to numeric, re-using roman to numeric.

--- src/main/java/org/galaxy/Merchant.java
+++ src/main/java/org/galaxy/Merchant.java
@@ -1,5 +1,6 @@
 package org.galaxy;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Merchant {
@@ -12,12 +13,13 @@ public class Merchant {
             "C", 100,
             "D", 500,
             "M", 1000);
+    private final Map<String, String> galacticToRomanMap = new HashMap<>();
 
-    public int convertToNumeral(String roman) {
+    public int convertToNumeral(String[] roman) {
         int result = 0;
         int lastValue = 0;
-        for (int i = roman.length() - 1; i >= 0; i--) {
-            int currentValue = romanToNumeralMap.get(String.valueOf(roman.charAt(i)));
+        for (int i = roman.length - 1; i >= 0; i--) {
+            int currentValue = romanToNumeralMap.get(String.valueOf(roman[i]));
             if (currentValue < lastValue) {
                 result -= currentValue;
             } else {
@@ -26,5 +28,17 @@ public class Merchant {
             lastValue = currentValue;
         }
         return result;
+    }
+
+    public void mapGalacticToRoman(String galactic, String roman){
+        galacticToRomanMap.put(galactic, roman);
+    }
+
+    public String[] galactic2Roman(String[] galactic){
+        String[] roman = new String[galactic.length];
+        for (int i = 0; i < galactic.length; i++) {
+            roman[i] = galacticToRomanMap.get(galactic[i]);
+        }
+        return roman;
     }
 }
--- src/test/java/org/galaxy/MerchantTest.java
+++ src/test/java/org/galaxy/MerchantTest.java
@@ -24,14 +24,44 @@ public class MerchantTest{
     @Test
     public void testRomanToNumeric(){
         Merchant merchant = new Merchant();
-        int result = merchant.convertToNumeral("MCMXLIV");
+        int result = merchant.convertToNumeral("MCMXLIV".split(""));
         TestCase.assertEquals(1944, result);
 
-        result = merchant.convertToNumeral("MMVI");
+        result = merchant.convertToNumeral("MMVI".split(""));
         TestCase.assertEquals(2006, result);
 
-        result = merchant.convertToNumeral("MCMIII");
+        result = merchant.convertToNumeral("MCMIII".split(""));
         TestCase.assertEquals(1903, result);
+
+        result = merchant.convertToNumeral("II".split(""));
+        TestCase.assertEquals(2, result);
+
+        result = merchant.convertToNumeral("XLII".split(""));
+        TestCase.assertEquals(42, result);
     }
+
+    @Test
+    public void testGalacticToNumeric(){
+        Merchant merchant = new Merchant();
+        merchant.mapGalacticToRoman("glob", "I");
+        merchant.mapGalacticToRoman("prok", "V");
+        merchant.mapGalacticToRoman("pish", "X");
+        merchant.mapGalacticToRoman("tegj", "L");
+
+        int result = merchant.convertToNumeral(merchant.galactic2Roman("glob glob".split(" ")));
+        TestCase.assertEquals(2, result);
+
+        result = merchant.convertToNumeral(merchant.galactic2Roman("glob prok".split(" ")));
+        TestCase.assertEquals(4, result);
+
+        result = merchant.convertToNumeral(merchant.galactic2Roman("pish pish".split(" ")));
+        TestCase.assertEquals(20, result);
+
+        result = merchant.convertToNumeral(merchant.galactic2Roman("pish tegj glob glob".split(" ")));
+        TestCase.assertEquals(42, result);
+
+    }
+
+
 
 }
