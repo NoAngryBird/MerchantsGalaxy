70b8eb16abaf17f0fd9ec0a97482542babef45dc
Hannes Helberg <hh@frei.one>
2023-11-15 23:26:37 +0200

    Added Main routine with full set of input text

--- src/main/java/org/galaxy/App.java
+++ src/main/java/org/galaxy/App.java
@@ -8,6 +8,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Merchant's Guide to the Galaxy!" );
+
+        String[] inputLines = {
+                "glob is I",
+                "prok is V",
+                "pish is X",
+                "tegj is L",
+                "glob glob Silver is 34 Credits",
+                "glob prok Gold is 57800 Credits",
+                "pish pish Iron is 3910 Credits",
+                "how much is pish tegj glob glob ?",
+                "how many Credits is glob prok Silver ?",
+                "how many Credits is glob prok Gold ?",
+                "how many Credits is glob prok Iron ?",
+                "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?"
+        };
+
+        Merchant merchant = new Merchant();
+        for (String line : inputLines) {
+            System.out.println(merchant.parseLine(line));
+        }
     }
 }
